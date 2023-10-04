976c6e6d2dd8b029a91d9b786fe11dd53c4d3c4d
Mark <Mark@b.com>
2023-10-04 18:44:14 +0800

    set menu

--- components/brandHeader/index.vue
+++ components/brandHeader/index.vue
@@ -9,47 +9,46 @@
           <span class="text-lg">{{ BRAND }}</span>
         </strong>
       </a>
-      <nav class="hidden content-center md:flex">
-        <NuxtLink class="home-nav-link" to="projects">Projects</NuxtLink>
-        <div class="home-nav-link">
+
+      <div class="absolute right-0 top-0 z-50 md:static">
+        <button @click="open = !open" class="relative h-8 w-8 focus:outline-none md:hidden">
+          <span class="sr-only">Open main menu</span>
+          <div
+            class="absolute left-1/2 top-1/2 block w-5 -translate-x-1/2 -translate-y-1/2 transform"
+          >
+            <span
+              aria-hidden="true"
+              class="absolute block h-0.5 w-5 transform bg-current transition duration-500 ease-in-out"
+              :class="{ 'rotate-45': open, ' -translate-y-1.5': !open }"
+            ></span>
+            <span
+              aria-hidden="true"
+              class="absolute block h-0.5 w-5 transform bg-current transition duration-500 ease-in-out"
+              :class="{ 'opacity-0': open }"
+            ></span>
+            <span
+              aria-hidden="true"
+              class="absolute block h-0.5 w-5 transform bg-current transition duration-500 ease-in-out"
+              :class="{ '-rotate-45': open, ' translate-y-1.5': !open }"
+            ></span>
+          </div>
+        </button>
+      </div>
+
+      <nav
+        :class="open ? 'translate-x-0' : 'translate-x-full'"
+        class="fixed bottom-0 right-0 top-0 flex w-full max-w-[150px] flex-col content-center gap-3 bg-zinc-200/95 pt-10 backdrop-blur-sm transition-all dark:bg-zinc-900/95 md:static md:w-auto md:translate-x-0 md:flex-row md:bg-transparent dark:md:bg-transparent md:pt-0"
+      >
+        <NuxtLink @click="open = false" class="home-nav-link px-3 md:hidden" to="/">
+          Home
+        </NuxtLink>
+        <NuxtLink @click="open = false" class="home-nav-link px-3" to="projects">
+          Projects
+        </NuxtLink>
+        <div class="home-nav-link px-3">
           <SwitchDarkMode></SwitchDarkMode>
         </div>
       </nav>
-
-      <!-- md:hidden -->
-      <button popovertarget="my-popover" title="Open menu" class="fill-current px-2">
-        <svg xmlns="http://www.w3.org/2000/svg" height="1rem" viewBox="0 0 448 512">
-          <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-          <path
-            d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
-          />
-        </svg>
-      </button>
-      <div
-        popover
-        id="my-popover"
-        class="m-0 h-full bg-zinc-100 p-0 pt-12 text-lg text-current dark:bg-zinc-800"
-      >
-        <button
-          popovertarget="my-popover"
-          title="Close menu"
-          class="absolute right-0 top-0 fill-current px-4 py-3 text-2xl"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
-            <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-            <path
-              d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
-            />
-          </svg>
-        </button>
-        <nav class="flex flex-col gap-2">
-          <NuxtLink class="home-nav-link mobile" to="/">Home</NuxtLink>
-          <NuxtLink class="home-nav-link mobile" to="projects">Projects</NuxtLink>
-          <div class="home-nav-link mobile">
-            <SwitchDarkMode></SwitchDarkMode>
-          </div>
-        </nav>
-      </div>
     </div>
   </header>
 </template>
@@ -60,6 +59,27 @@ const config = useRuntimeConfig();
 
 const url = config.public.BASE_URL;
 const BRAND = config.public.BRAND_NAME;
+
+const open = ref(false);
+const hideWrap = ref(true);
+const REF_DRAWER_WRAP = ref<HTMLDivElement | null>(null);
+const initDrawerEnd = () => {
+  hideWrap.value = true;
+  console.log('end');
+};
+const initDrawerStart = () => {
+  hideWrap.value = false;
+  console.log('start');
+};
+onMounted(() => {
+  REF_DRAWER_WRAP.value?.addEventListener('animationend', initDrawerEnd);
+  // REF_DRAWER_WRAP.value?.addEventListener('transitionstart', initDrawerStart);
+});
+
+onUnmounted(() => {
+  // REF_DRAWER_WRAP.value?.removeEventListener('transitionend', initDrawerEnd);
+  // REF_DRAWER_WRAP.value?.removeEventListener('transitionstart', initDrawerStart);
+});
 </script>
 
 <style lang="postcss" scoped>
@@ -68,7 +88,7 @@ const BRAND = config.public.BRAND_NAME;
 }
 
 .home-nav-link {
-  @apply mx-1 p-1 transition-colors;
+  @apply transition-colors md:mx-1 md:p-1;
 
   &.mobile {
     @apply px-3;
@@ -101,12 +121,21 @@ const BRAND = config.public.BRAND_NAME;
   }
 }
 
-@keyframes transIn {
+.animation.active {
+  animation-duration: 2s;
+  animation-name: slidein;
+  animation-iteration-count: 2;
+}
+
+@keyframes slidein {
   from {
-    translate: 100%;
+    margin-left: 100%;
+    width: 300%;
   }
+
   to {
-    translate: 0%;
+    margin-left: 0%;
+    width: 100%;
   }
 }
 </style>
