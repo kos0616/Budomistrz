2f1d7266d1e7162fefe3b3b465a2b556ec2259aa
Mark <Mark@b.com>
2023-10-06 13:51:08 +0800

    add dynamic news pages

---
+++ pages/news/[id].vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="p-10">
+    <Breadcrumb v-model="breadcrumbs" class="mb-3" />
+    <div class="container mx-auto">
+      <article
+        v-skeleton="loading"
+        itemscope
+        itemtype="https://schema.org/Article"
+        class="bg-zinc-300/75 p-5 dark:bg-zinc-800/75"
+      >
+        <h1 v-skeleton-item itemprop="name" class="mb-4">
+          {{ info?.title }}
+        </h1>
+
+        <div
+          itemtype="https://schema.org/Person"
+          itemprop="author"
+          itemscope
+          class="flex items-center gap-2 py-2"
+        >
+          <img
+            src="http://placebeard.it/50/notag"
+            itemprop="image"
+            :alt="`photo of ${author.name}`"
+            class="rounded-full"
+          />
+          <div>
+            Author: <span itemprop="name">{{ author.name }}</span>
+            <a
+              itemprop="email"
+              :href="`mailto:${author.email}`"
+              class="mx-2 inline-block fill-current align-middle"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+                <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                <path
+                  d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+
+        <figure v-if="photos[0].url" class="mb-10">
+          <img
+            itemprop="image"
+            height="450"
+            width="450"
+            :src="photos[0].url"
+            class="aspect-video object-cover"
+          />
+          <figcaption>{{ photos[0].title }}</figcaption>
+        </figure>
+
+        <p v-skeleton-item itemprop="articleBody" class="mb-16">{{ info?.body }}</p>
+
+        <ol class="flex flex-wrap gap-2">
+          <li v-for="(img, i) in photos" :key="`img_${i}`">
+            <a :href="img.url" target="_blank">
+              <img itemprop="image" :src="img.thumbnailUrl" :alt="img.title" />
+            </a>
+          </li>
+        </ol>
+      </article>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Breadcrumb } from '#components';
+const route = useRoute();
+const { id } = route.params;
+
+const breadcrumbs = ref([
+  { name: 'Home', value: '/' },
+  { name: 'News', value: '/news' }
+]);
+const info = ref<{ body: string; id: number; title: string; userId: number } | {}>({});
+
+const author = ref<{
+  id: 1;
+  name: 'Leanne Graham';
+  username: 'Bret';
+  email: 'Sincere@april.biz';
+  address: {
+    street: 'Kulas Light';
+    suite: 'Apt. 556';
+    city: 'Gwenborough';
+    zipcode: '92998-3874';
+    geo: {
+      lat: '-37.3159';
+      lng: '81.1496';
+    };
+  };
+  phone: '1-770-736-8031 x56442';
+  website: 'hildegard.org';
+  company: {
+    name: 'Romaguera-Crona';
+    catchPhrase: 'Multi-layered client-server neural-net';
+    bs: 'harness real-time e-markets';
+  };
+}>({});
+
+const loading = ref(false);
+
+const setSeo = () => {
+  useJsonld({
+    '@context': 'https://schema.org',
+    '@type': 'BreadcrumbList',
+    itemListElement: [
+      {
+        '@type': 'ListItem',
+        position: 1,
+        name: 'news',
+        item: 'https://kos0616.github.io/Budomistrz/news'
+      }
+    ]
+  });
+};
+
+const getInfo = async () => {
+  loading.value = true;
+  const { data } = await useFetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  info.value = data.value;
+  loading.value = false;
+};
+
+const getAuthor = async () => {
+  const { data } = await useFetch(
+    `https://jsonplaceholder.typicode.com/users/${info.value.userId}`
+  );
+  author.value = data.value;
+};
+
+const photos = ref<
+  {
+    albumId: 1;
+    id: 1;
+    title: 'accusamus beatae ad facilis cum similique qui sunt';
+    url: 'https://via.placeholder.com/600/92c952';
+    thumbnailUrl: 'https://via.placeholder.com/150/92c952';
+  }[]
+>([]);
+
+const getPhotos = async () => {
+  const { data } = await useFetch(`https://jsonplaceholder.typicode.com/photos?albumId=${id}`);
+  photos.value = data.value;
+};
+
+setSeo();
+await getInfo();
+await getAuthor();
+await getPhotos();
+</script>
---
+++ pages/news/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <article class="container mx-auto">
+    <Breadcrumb v-model="breadcrumbs" class="my-3" />
+    <h1 class="mb-10 border-b pb-3">News</h1>
+    <ClientOnly>
+      <div
+        class="mx-auto mb-10 flex max-w-xl flex-col gap-2 bg-zinc-300/75 p-5 dark:bg-zinc-800/75"
+      >
+        <section
+          v-for="(p, i) in list"
+          :key="`news_${p.id}`"
+          class="border-b border-zinc-300/25 py-3 last:border-0"
+        >
+          <h2>
+            {{ p.title }}
+          </h2>
+          <p class="truncate">{{ p.body }}</p>
+          <div class="text-right">
+            <NuxtLink
+              :to="`/news/${p.id}`"
+              :title="p.title"
+              class="text-primary-400 hover:text-primary-500 hover:underline"
+            >
+              MORE
+            </NuxtLink>
+          </div>
+        </section>
+      </div>
+    </ClientOnly>
+  </article>
+</template>
+
+<script lang="ts" setup>
+import { Breadcrumb } from '#components';
+
+const breadcrumbs = ref([
+  { name: 'Home', value: '/' },
+  { name: 'News', value: '/news' }
+]);
+
+const list = ref<{ body: string; id: number; title: string; userId: number }[]>([]);
+
+const getLists = async () => {
+  const { data } = await useFetch('https://jsonplaceholder.typicode.com/posts');
+  list.value = data.value;
+};
+
+getLists();
+</script>
