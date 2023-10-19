68e4e672fabdcc9f24c9de00b64298e7eb84b39c
Mark <Mark@b.com>
2023-10-19 23:33:46 +0800

    swipper add autoplay

--- plugins/vue-swiper.ts
+++ plugins/vue-swiper.ts
@@ -1,12 +1,12 @@
 import { defineNuxtPlugin } from 'nuxt/app';
 import VueAwesomeSwiper from 'vue-awesome-swiper';
 /** import pagination if need */
-import SwiperClass, { Navigation } from 'swiper';
+import SwiperClass, { Navigation, Autoplay } from 'swiper';
 import 'swiper/css';
 // import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 export default defineNuxtPlugin((nuxtApp) => {
-  SwiperClass.use([Navigation]);
+  SwiperClass.use([Navigation, Autoplay]);
   nuxtApp.vueApp.use(VueAwesomeSwiper);
 });
